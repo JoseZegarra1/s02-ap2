9c72be6b6b879763457326f63dc793b403e70b45
Jose Zegarra <jose.zegarra@vallegrande.edu.pe>
2024-05-12 21:35:37 -0500

    Array2

---
+++ src/Actividad2_12_Array2.java
@@ -0,0 +1,10 @@
+public class Actividad2_12_Array2 {
+    public static void main(String[] args) {
+        String[] colores = {"Rojo", "Verde", "Azul", "Blanco", "Negro"};
+
+        System.out.println("Lista de colores:");
+        for (int i = 0; i < colores.length; i++) {
+            System.out.println(colores[i]);
+        }
+    }
+}
