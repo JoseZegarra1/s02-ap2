3517477fa579085e232a6d4815b77351ef3ac7db
Jose Zegarra <jose.zegarra@vallegrande.edu.pe>
2024-05-12 21:40:08 -0500

    While

---
+++ src/Actividad2_12_While.java
@@ -0,0 +1,11 @@
+public class Actividad2_12_While {
+    public static void main(String[] args) {
+        int numero = 1;
+
+        System.out.println("Números del 1 al 5:");
+        while (numero <= 5) {
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
